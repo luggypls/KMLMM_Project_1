80de035235352b80363083628b92066fd3b75109
luggypls <lluis.grifols@gmail.com>
2021-12-05 21:18:28 +0100

    add kernel pca

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
 import loading as l
 import gridsearch as g
 
-from sklearn.svm import SVC
-from sklearn.metrics import make_scorer, roc_auc_score, recall_score
+import numpy as np
+from sklearn.svm import SVC,LinearSVC
+from sklearn.metrics import make_scorer, roc_auc_score, recall_score, accuracy_score,confusion_matrix, precision_score
+
+from sklearn.model_selection import train_test_split
+
 
 data_path='./Data/pd_speech_features.csv'
 
 X,y = l.load_data(data_path)
 
-roc_scorer = make_scorer(roc_auc_score)
-recall_scorer = make_scorer(recall_score)
-
-metrics = {'accuracy': 'accuracy',''
-           'roc': roc_scorer,
-           'recall': recall_scorer
-           }
-
-model_params = {'kernel' : ['linear', 'rbf', 'poly'],
-                'C' : [1, 10],
-                'degree' : [2, 3, 10],
-                'gamma' : ['scale', 'auto']
-                }
-
-clf=SVC()
-out=g.GridSearch(X,y,clf, metrics, model_params)
-print(out.get_results)
